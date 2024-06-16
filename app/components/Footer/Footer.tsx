a511f6714a9a4c10b18c7b8a70eb719c9b85bd73
samirkhanalofficial <samirkhanalofficial@gmail.com>
2024-06-16 17:03:46 +0545

    added developed by

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -114,16 +114,18 @@ const footer = () => {
       <div className="py-10 md:flex items-center justify-between border-t border-t-gray-blue">
         <h4 className="text-dark-red opacity-75 text-sm text-center md:text-start font-normal">
           @{new Date().getFullYear()}.NebulaCare.All rights reserved
+          <br />
+          Developed By <Link href={"https://aideascent.com"}>Aide Ascent</Link>
         </h4>
         <div className="flex gap-5 mt-5 md:mt-0 justify-center md:justify-start">
           <h4 className="text-dark-red opacity-75 text-sm font-normal">
-            <Link href="/" target="_blank">
+            <Link href="#" target="_blank">
               Privacy policy
             </Link>
           </h4>
           <div className="h-5 bg-dark-red opacity-25 w-0.5"></div>
           <h4 className="text-dark-red opacity-75 text-sm font-normal">
-            <Link href="/" target="_blank">
+            <Link href="#" target="_blank">
               Terms & conditions
             </Link>
           </h4>
